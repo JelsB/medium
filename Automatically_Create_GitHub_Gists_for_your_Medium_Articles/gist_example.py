926d244ea143441f3b6ce82bf0a64d91e5ad680c
JelsB <boulangier.jels@gmail.com>
2021-01-26 23:34:24 +0100

    Add gists for new article about gists

---
+++ Automatically_Create_GitHub_Gists_for_your_Medium_Articles/gist_example.py
@@ -0,0 +1,12 @@
+like_this = True
+
+def the_adavantage(of_a_GitHub_gist):
+    is_that_it_shows = 'your code with coloured highlights.'
+    depending_on_the_file_type = '(which is Python in this example),'
+    the_highlighting_is_different = False
+
+    additionally_it_allows_you_to_scroll_horizontally_inside_the_gist = 'Something which is not possible for plain code blocks.'
+
+    hence_gists_add_more = f'lexibility to your snippet, {like_this}'
+
+    print('Look! It\'s pretty!')
